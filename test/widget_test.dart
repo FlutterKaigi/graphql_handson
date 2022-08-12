be67eb4d777b8e40efc2ac524d2f0fcee98b6230
jiyuujin <jiyuujinunite@gmail.com>
2022-08-12 19:18:09 +0900

    test passed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,14 @@
 // This is a basic widget test that tests just the good app code.
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:graphql_handson/goodapp.dart';
+import 'package:graphql_handson/main.dart';
 
 void main() {
   testWidgets('App smoke test', (tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const GoodMyApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that we have a forecast, or a managed failure.
-    expect(find.textContaining('Temperature'), findsOneWidget);
+    expect(find.textContaining('Flutter Demo Home Page'), findsOneWidget);
   });
 }
