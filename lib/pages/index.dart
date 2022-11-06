e357821caec8ab8963a986c10442759967d8b2b7
Tatsuya Kainuma <numatch@jxpress.net>
2022-11-06 22:33:31 +0900

    :lipstick: デザインをFigmaに近づける

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -11,61 +11,52 @@ class MyTopPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FutureBuilder<dynamic>(
-        future: (showRepository && !showIssue)
-            ? fetchRepositories()
-            : fetchIssues(),
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.active:
-            case ConnectionState.waiting:
-              return const Text('Loading');
-            case ConnectionState.done:
-              if (snapshot.hasError) {
-                return Center(child: Text('Error : ${snapshot.error}'));
-              }
+    return FutureBuilder<dynamic>(
+      future:
+          (showRepository && !showIssue) ? fetchRepositories() : fetchIssues(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.active:
+          case ConnectionState.waiting:
+            return const Text('Loading');
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return Center(child: Text('Error : ${snapshot.error}'));
+            }
 
-              if (snapshot.data == null) {
-                return const Text('No issues');
-              }
+            if (snapshot.data == null) {
+              return const Text('No issues');
+            }
 
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, index) {
-                  if ((showRepository && !showIssue)) {
-                    // Repository一覧の表示
-                    final Repository repository = snapshot.data[index];
-                    return Container(
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      child: CardItem(
-                        title: repository.name,
-                        message: repository.description ?? '',
-                        url: repository.url,
-                        updatedAt: repository.updatedAt,
-                      ),
-                    );
-                  } else {
-                    // Issue一覧の表示
-                    final Issue issue = snapshot.data[index];
-                    return Container(
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      child: CardItem(
-                        id: issue.id,
-                        title: issue.title,
-                        message: issue.body ?? '',
-                        url: issue.url,
-                        updatedAt: issue.updatedAt,
-                      ),
-                    );
-                  }
-                },
-              );
-          }
-        },
-      ),
+            return ListView.builder(
+              shrinkWrap: true,
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                if ((showRepository && !showIssue)) {
+                  // Repository一覧の表示
+                  final Repository repository = snapshot.data[index];
+                  return CardItem(
+                    title: repository.name,
+                    message: repository.description ?? '',
+                    url: repository.url,
+                    updatedAt: repository.updatedAt,
+                  );
+                } else {
+                  // Issue一覧の表示
+                  final Issue issue = snapshot.data[index];
+                  return CardItem(
+                    id: issue.id,
+                    title: issue.title,
+                    message: issue.body ?? '',
+                    url: issue.url,
+                    updatedAt: issue.updatedAt,
+                  );
+                }
+              },
+            );
+        }
+      },
     );
   }
 }
@@ -90,7 +81,7 @@ class CardItem extends StatelessWidget {
     return Directionality(
       textDirection: TextDirection.ltr,
       child: Card(
-        color: Colors.white70,
+        color: Colors.white,
         child: InkWell(
           onTap: () async {
             if (id == '') {
@@ -112,38 +103,35 @@ class CardItem extends StatelessWidget {
               );
             }
           },
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.only(top: 4),
-                  child: Text(
-                    title,
-                    style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87),
-                  ),
-                ),
-                Text(
-                  message,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(top: 4),
+                child: Text(
+                  title,
                   style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.normal,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                       color: Colors.black87),
                 ),
-                Text(
-                  updatedAt,
-                  style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black87),
-                ),
-              ],
-            ),
+              ),
+              Text(
+                message,
+                style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black87),
+              ),
+              Text(
+                updatedAt,
+                style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black87),
+              ),
+            ],
           ),
         ),
       ),
